76d07190c90f9172e5affb625cf8f8a4e4f91f4f
Rico Zhu <ricozhuthegreat@hotmail.com>
2022-02-06 07:57:30 -0500

    Completed Classic SEIR Model

--- seir.py
+++ seir.py
@@ -64,3 +64,55 @@ class SEIR:
         params.add("rho", expr='gamma', vary=False)
 
         return params
+
+    def get_initial_conditions(self, data):
+        # Simulate and adjust initial params to match deaths to data
+        population = self.params['population']
+        epidemic_started_days_ago = self.params['epidemic_started_days_ago']
+
+        t = np.arange(epidemic_started_days_ago)
+        (S, E, I, R, D) = self.predict(t, (population - 1, 0, 1, 0, 0))
+
+        I0 = I[-1]
+        E0 = E[-1]
+        Rec0 = R[-1]
+        D0 = D[-1]
+        S0 = S[-1]
+        return (S0, E0, I0, Rec0, D0)
+
+    def step(self, initial_conditions, t):
+
+        # Function to be solved via odeint
+
+        population = self.params['population']
+        delta = self.params['delta']
+        gamma = self.params['gamma']
+        alpha = self.params['alpha']
+        rho = self.params['rho']
+        
+        rt = self.params['r0'].value
+        beta = rt * gamma
+
+        S, E, I, R, D = initial_conditions
+
+        # Insert neural network here:
+
+        new_exposed = beta * I * (S / population)
+        new_infected = delta * E
+        new_dead = alpha * rho * I
+        new_recovered = gamma * (1 - alpha) * I
+
+        dSdt = -new_exposed
+        dEdt = new_exposed - new_infected
+        dIdt = new_infected - new_recovered - new_dead
+        dRdt = new_recovered
+        dDdt = new_dead
+
+        assert S + E + I + R + D - population <= 1e10
+        assert dSdt + dIdt + dEdt + dRdt + dDdt <= 1e10
+        return dSdt, dEdt, dIdt, dRdt, dDdt
+
+    def predict(self, t_range, initial_conditions):
+        ret = odeint(self.step, initial_conditions, t_range)
+        return ret.T
+
